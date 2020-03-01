b042d94fae588422d66a5d477a62de949f86f304
Erik Moqvist <erik.moqvist@gmail.com>
2020-03-01 13:42:47 +0100

    Moved files from monolinux repo.

---
+++ setup.sh
@@ -0,0 +1,23 @@
+cd monolinux
+source setup.sh
+cd ..
+
+# Monolinux root folder.
+export ML_ROOT=$(readlink -f monolinux)
+
+# Linux kernel configuration.
+export ML_LINUX_CONFIG=(readlink -f configs/x86_64-default.config)
+
+export PATH=$PATH:$ML_ROOT/bin
+export PATH=$(readlink -f x86_64-linux-musl-cross/bin):$PATH
+
+# Cross compilation.
+
+export CROSS_COMPILE=x86_64-linux-musl-
+# autotools: The system where built programs and libraries will run.
+export ML_AUTOTOOLS_HOST=x86_64-linux-musl
+
+# For ARM.
+# export ARCH=arm
+# export CROSS_COMPILE=arm-linux-musleabi-
+# export ML_AUTOTOOLS_HOST=arm-linux-musleabi
