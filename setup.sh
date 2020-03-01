236c69acd29256a14d5be4523a5808bf78b97fbb
Erik Moqvist <erik.moqvist@gmail.com>
2020-03-01 15:28:41 +0100

    Update monolinux subrepo.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,7 @@
-# Monolinux root folder.
 export ML_ROOT=$(readlink -f 3pp/monolinux)
-
-# Sources folder.
 export ML_SOURCES=$(readlink -f 3pp)
-
-# Linux kernel configuration.
 export ML_LINUX_CONFIG=$(readlink -f configs/linux-x86_64.config)
-
 export PATH=$PATH:$ML_ROOT/bin
 export PATH=$(readlink -f x86_64-linux-musl-cross/bin):$PATH
-
-# Cross compilation.
-
 export CROSS_COMPILE=x86_64-linux-musl-
-# autotools: The system where built programs and libraries will run.
 export ML_AUTOTOOLS_HOST=x86_64-linux-musl
-
-# For ARM.
-# export ARCH=arm
-# export CROSS_COMPILE=arm-linux-musleabi-
-# export ML_AUTOTOOLS_HOST=arm-linux-musleabi
